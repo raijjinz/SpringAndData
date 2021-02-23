c1173667cc70333ca0d28520e2584f87145922da
Chaitanya Jain <chaitanyajain@Chaitanyas-MacBook-Pro.local>
2021-02-23 14:13:30 +0530

    boilerplate

---
+++ src/main/java/com/example/PostgreSpringDemo/PostgreSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.PostgreSpringDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgreSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgreSpringDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PostgreSpringDemo/PostgreSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PostgreSpringDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgreSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
